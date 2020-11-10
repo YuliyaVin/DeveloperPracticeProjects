2140ef12b7b1bf3a41d3a899d3d87c6e3f438d91
Yuliya <wwwlfire@hotmail.com>
2020-11-10 09:32:56 -0500

    add new folder

---
+++ CodeChallenge1_GettingDataFromAnAPI/CodeChallenge1_GettingDataFromAnAPI_Ver1/CodeChallenge1_GettingDataFromAnAPI/Controllers/CreditCardAPIController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeChallenge1_GettingDataFromAnAPI.Helpers;
+using Newtonsoft.Json;
+
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace CodeChallenge1_GettingDataFromAnAPI.Controllers
+{
+    public class CreditCardAPIController
+    {
+    public  string Token { get; set; }
+    public  string User { get; set; }
+
+        public  async Task<List<string>> GetTerminalIdentifiers()
+        {
+            try
+            {
+
+        
+            var client = new HttpClient();
+            var authToken = Encoding.ASCII.GetBytes($"{User}:{Token}");
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", Convert.ToBase64String(authToken));
+           
+            var response = await client.PostAsync("https://ccm.cps.golf/RegressionSRCCM/Transactions.svc/GetTerminalIdentifiers", null);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+
+            return !string.IsNullOrEmpty(content) ?
+               JsonConvert.DeserializeObject<List<String>>(content) :
+               new List<string>();
+           // return JsonConvert.DeserializeObject<List<String>>(content); 
+            }
+            catch (Exception e){
+                Console.WriteLine(e.ToString());
+                return new List<string>();
+            }
+
+        }
+    }
+}
